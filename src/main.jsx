7f46094622fac32e996ac83d74d88abb74d3cb0d
sophia-rhim <soyun.sophia.rhim@gmail.com>
2026-04-19 11:18:23 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
@@ -10,12 +10,10 @@ const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ConvexProvider client={convex}>
-      <ConvexAuthProvider client={convex}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ConvexAuthProvider>
-    </ConvexProvider>
+    <ConvexAuthProvider client={convex}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConvexAuthProvider>
   </StrictMode>
 );
