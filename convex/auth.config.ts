fee6a20a65f20acd0f2494891cc466acb0e00df9
sophia-rhim <soyun.sophia.rhim@gmail.com>
2026-04-19 10:51:56 -0700

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from "convex/server";
 export default {
   providers: [
     {
-      domain: process.env.CONVEX_SITE_URL!, 
+      domain: process.env.CONVEX_SITE_URL ?? "https://grateful-dragon-149.convex.site",
       applicationID: "convex",
     },
   ],
