4a692866f5f80508ce36681b3730b5041871f457
gemma <gemmayun0907@gmail.com>
2026-04-19 18:13:07 +0000

    Resolve merge conflict in auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,9 +1,13 @@
 import { AuthConfig } from "convex/server";
-
+SS
 export default {
   providers: [
     {
+<<<<<<< Updated upstream
       domain: "https://grateful-dragon-149.convex.site",
+=======
+      domain: "https://fearless-pigeon-561.convex.site/",
+>>>>>>> Stashed changes
       applicationID: "convex",
     },
   ],
